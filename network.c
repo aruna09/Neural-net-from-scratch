556e98656bf1e5d8203fd0442841a03f61e4d538
ArunaMaurya221B <aruna.maurya12@gmail.com>
2018-12-30 23:32:56 +0530

    Trying to code a neural net from scratch. Wrote function to compute the product of feature and weight matrix, along with the activation function

---
+++ network.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+//declaring nodes
+struct node{
+	int weights[100];
+	int feature_values[100];
+	int bias;
+};
+
+//function to init data, i.e features and labels.
+void generate_data(){
+	//we are gonna have a 10X100 matrix,where each row corresponds 
+	//to the laid out pixel vales and the corresponding labels for it.
+}
+void init_data(){
+	//this is where we initialize the weights and the feature_value array;
+}
+ 
+void main(){
+	generate_data()
+	init_data()
+}
