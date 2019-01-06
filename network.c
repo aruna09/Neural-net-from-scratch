ecd0484abc98d59b4503a57160a33df693339c31
ArunaMaurya221B <aruna.maurya12@gmail.com>
2019-01-06 13:58:47 +0530

    Had to redo most of the stuff done during last time

--- network.c
+++ network.c
@@ -3,49 +3,58 @@
 #include <math.h> //gcc -o net net.c -lm to link math library
 #define no_of_nodes 10
 #define no_of_layers 5 
+#define size_of_training_set 1000
 
 typedef struct 
 {
 	int x;
 	double weights[no_of_nodes];
-	double weight_last_layer;
+	double weight_last_layer[10];
 }n ;
 
-//used to initialize the network with the no of layers and nodes.
-void init_network(/*pass and return structure objects here*/){
 	/*
+
+	Used to initialize the network with the no of layers and nodes.
 	If there are 6 layers(5(ip) + 1(op)) we need 4 weight vectors.
 	No weight vector for the last but one layer as it is connected to
-	only one neuron.
+	only ten neuron. The weight array will be assigned separately.
+
 	*/
 	n nodes[no_of_layers-1][no_of_nodes];
-}
+	double sum[no_of_layers][no_of_nodes];
+	double error[size_of_training_set];
+	double predicted[size_of_training_set];
+	int label[size_of_training_set];//read from training set
+	double bias_last_layer;
+	int index;//for recording the predicted value for each sample
+	double learning_rate = 0.5;//change this.
 
-//function to initialize bias and weight values
 void initialize_values(n node_struct){
 
 	/*
-	Issue:
-	1. With how you pass a matrix of structures. 
-		Once corrected add code here.
-	2. Initialize x[] values for all layers by reading from training set
+	
+	Function to initialize bias and weight values.
 
-	initialize_values(nodes);
+	Issue:
+	1. Initialize x[] values for all layers by reading from training set
+
+	Initialize_values
+	Initializing the bias.
+	Initializing the weights
+	
 	*/
 
 	int i, j, k;
 	
-	for(i=0;i<no_of_nodes;i++)
-		nodes[0][i].x = 1;//2
+	for(i = 0; i < no_of_nodes; i++)
+		nodes[0][i].x = 1;
 
-	//initializing the bias.
-	for(i=0;i<no_of_layers-2;i++)
+	for(i = 0; i < no_of_layers-2; i++)
 		bias[i] = rand() % 10;
 
-	//initializing the weights
-	for(i=0;i<no_of_layers-1;i++){
-		for(j=0;j<no_of_nodes;j++){
-			for(k=0;k<no_of_nodes;k++){
+	for(i = 0; i < no_of_layers-1; i++){
+		for(j = 0; j < no_of_nodes; j++){
+			for(k = 0; k < no_of_nodes; k++){
 				nodes[i][j].weights[j] = rand() % 1;
 			}
 		}
@@ -53,34 +62,45 @@ void initialize_values(n node_struct){
 	
 }
 
-//sample function to perform activation
+
 double sigmoid(double x){
+
+	/*
+
+	Sample function to perform activation
+
+	*/
 	return(1/1+exp(-x));
 }
 
-//function to computer derivative to compute cost
-doub sigmoid_derivative(double x){
+
+double sigmoid_derivative(double x){
+	/*
+
+	Function to computer derivative to compute cost.
+
+	*/
+
 	return(sigmoid(x) * (1-sigmoid(x)));
 }
 
 
 void dot_product(){
+
+
 	/*
-	figure out a way to pass structures, until then 
-	the function defination is in the main()
+
+	Function to compute the dot product: W.X + b
+
 	*/
-	double sum[no_of_layers][no_of_nodes];
-
-
-	//compute the dot product: x.w + b;
-	for(i=0;i<no_of_layers;i++){
-		for(j=0;j<no_of_nodes;j++){
-			for(k=0;k<no_of_nodes;k++){
+	for(i = 0; i < no_of_layers; i++){
+		for(j = 0; j < no_of_nodes; j++){
+			for(k = 0; k < no_of_nodes; k++){
 				sum[i][k] += nodes[i][j].x * nodes[i][j].weights[k];
 			}
 		}
-		for(a=0;a<no_of_nodes;a++){
-			sum[i][a] = sum[i][a]+bias[i];
+		for(a = 0; a < no_of_nodes; a++){
+			sum[i][a] = sum[i][a] + bias[i];
 			sum[i][a] = sigmoid(sum[i][a]);
 			nodes[i][a].x = sum[i][a];
 		}
@@ -88,50 +108,99 @@ void dot_product(){
 
 }
 
-//function to compute the error
-double* cost_function(double predicted[], int label[], int l){
-	//computing the MSE
-	double error[l];
-	//size of predicted and label is same. let that be l.
-	for(i=0;i<l;i++){
-		error[i] = pow(predicted[i]-label[i], 2);
-	}
-	//returning an array. correct the type.
+
+double cost_function(double predicted, double label){
+
+	/*
+
+	Function to compute the error.
+	Computing MSE
+
+	*/
+
+	error = pow(predicted-label, 2);
+	
 	return error;	
 }
 
 void feed_forward(){
+
+	/*
+
+	Function to carry out forward propagation.
+	 Calls dot_product to do the needful.
+
+	*/
+
 	dot_product();
 }
 
 void back_prop(){
-	double sum;
 	/*
-	bias for the last layer.
-	assuming that I am working wth the last but one layer.
-	have to call this for all examples in training data.
+
+	Function to carry out backward propagation.
+	Calls dot_product to do the needful.
+
 	*/
-	double bias = rand() % 2;
-	int index;//index no of the training data
-		for(j=0;j<no_of_nodes;j++){
-			sum += (nodes[no_of_layers-1][j].x * nodes[no_of_layers-1][j].weight_last_layer) + bias;
-			dpred_dz[i] = sigmoid_der(sum);
+
+	int j;
+	/*
+
+	Bias for the last layer.
+	Assuming that I am working wth the last but one layer.
+
+	*/
+	
+	
+		for(j = 0; j < no_of_nodes; j++){
+			for(k = 0; k < 10; k++){
+				sum += (nodes[no_of_layers-1][j].x * nodes[no_of_layers-1][j].weight_last_layer[k]) + bias_last_layer;
+				nodes[no_of_layers][k].x += sum;
+				dpred_dz[k] = sigmoid_der(sum);
+			}
 		}
-		predicted[index] = sigmoid(sum);
-		//call this only after all the training examples are done.
-		//predicted and label are two arrays and error function computes th error value
-		//for each prediction. Error is also an array
-		error = cost_function(predicted, label);
+
+		/*
+
+		Assuming that after each forward prop for a particular 
+		training sample, backword prop takes place, which essentially 
+		means that error is not an array. It is a single double value.
+
+		*/
+		int i;//index for mapping the labels
+
+		for(j = 0; j < 10; j++){
+			predicted[j] = sigmoid(nodes[no_of_layers-1][j].x);
+			/* 
+
+			Here a sigmoid layer is being used. i.e with 10 neurons in 
+			the last layer, the output would be an array of 0's and 1's
+			where the corresponding index to a 1 will be the respective number 
+
+			*/
+
+			// somevalue is the threshold value for node to get activated.
+			if(predicted[j] > somevalue){
+				number  = j;
+				/*
+
+				do something with label, label has been initialized as an 
+				array but taking it as a number sounds more logical.
+				something like label[i]
+
+				*/
+				error = cost_function(number, label[i]);
+			}
+			i++;
+		}
 		
-		for(i=0;i<length_of_error;i++){
+		for(i = 0; i < size_of_training_set; i++){
 			delta[i] = error[i] * dpred_dz[i];
 		}
 		
-
-		//transposing the input x to multiply
-		double learning_rate = 0.5;//change this.
-		for(i=0;i<no_of_layers;i++){
-			for(j=0;j<no_of_nodes;j++){
+		
+		for(i = 0; i < no_of_layers; i++){
+			for(j = 0; j < no_of_nodes; j++){
 				matrix_transpose[j][i] = nodes[i][j].weights[j];
 				nodes[i][j].weights[j] -= learning_rate * matrix_transpose[j][i] * delta[j];
 			}
@@ -148,9 +217,14 @@ void main(){
 	*/
 
 
-	//function to call the dot_product function.
-	// The defination above is dot_product's.
-	for(i=0;i<200;i++){
+	/*
+
+	For each training sample run the forward and back prop atleast 200 times.
+	That's the reason as rk why we 
+
+	*/
+
+	for(i=0;i<size_of_training_set;i++){
 		feed_forward();
 		back_prop();
 	}
